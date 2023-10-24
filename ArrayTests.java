c05408f4e26663dcaf062d712388e1983690c258
Jared Rosas <jaredsbt@gmail.com>
2023-10-24 11:23:26 -0700

    Fixed ArrayExamples

--- ArrayTests.java
+++ ArrayTests.java
@@ -15,4 +15,17 @@ public class ArrayTests {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
   }
+  
+  @Test
+  public void testReverseInPlaceBigArray() {
+	  int[] input1 = { 3, 2, 1 };
+	  ArrayExamples.reverseInPlace(input1);
+	  assertArrayEquals(new int[]{ 1, 2, 3 }, input1);
+  }
+  
+  @Test
+  public void testAverageWithoutLowest() {
+	  double[] input1 = { 5.0 , 2.0, 6.0 , 7.0, 0.0};
+	  assertEquals(5.0, ArrayExamples.averageWithoutLowest(input1), 0.0);
+  }
 }
